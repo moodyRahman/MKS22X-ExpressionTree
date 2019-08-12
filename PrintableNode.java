0b396b8601dda4ab52d63e0f8eecac426b8bcc39
Moody Rahman <mrahman01@stuy.edu>
2019-08-12 07:15:53 -0400

    printable node interface

---
+++ PrintableNode.java
@@ -0,0 +1,14 @@
+/** Node that can be printed */
+public interface PrintableNode
+{
+    /** Get left child */
+    PrintableNode getLeft();
+
+
+    /** Get right child */
+    PrintableNode getRight();
+
+
+    /** Get text to be printed */
+    String getText();
+}
